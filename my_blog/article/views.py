0461e440b149a8632e4273664cb07b43512b627a
yaoxipeng <yaoxipeng666@126.com>
2022-01-17 15:05:00 +0800

    使用Markdown语法书写文章

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-# 导入 HttpResponse 模块
-# from django.http import HttpResponse
-# 导入数据模型ArticlePost
+# 引入markdown模块
+import markdown
 from .models import ArticlePost
 
 
@@ -18,15 +15,18 @@ def article_list(request):
     # render 函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
 
-# 文章详情
+
 def article_detail(request, id):
-    # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
-    # 需要传递给模板的对象
-    context = { 'article': article }
-    # 载入模板，并返回context对象
+
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body,
+                                     extensions=[
+                                         # 包含 缩写、表格等常用扩展
+                                         'markdown.extensions.extra',
+                                         # 语法高亮扩展
+                                         'markdown.extensions.codehilite',
+                                     ])
+
+    context = {'article': article}
     return render(request, 'article/detail.html', context)
-
-
-
-
