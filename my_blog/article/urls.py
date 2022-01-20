5f7050a019e29bc2e06fb2600ee709a4c0c9b4c9
yaoxipeng <yaoxipeng666@126.com>
2022-01-20 12:04:59 +0800

    完成修改文章功能

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
         views.article_safe_delete,
         name='article_safe_delete'
     ),
+    # 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update')
 ]
