86064c0e1a0c283a8e8c37df2cfac349c438b908
yaoxipeng <yaoxipeng666@126.com>
2022-01-20 11:38:48 +0800

    新增了删除博客文章的功能，并且使用了弹窗组件优化了用户体验,添加CSRF攻击的防范

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -14,5 +14,11 @@ urlpatterns = [
     # 写文章
     path('article-create/', views.article_create, name='article_create'),
     # 删除文章
-    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    # path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    # 安全删除文章
+    path(
+        'article-safe-delete/<int:id>/',
+        views.article_safe_delete,
+        name='article_safe_delete'
+    ),
 ]
