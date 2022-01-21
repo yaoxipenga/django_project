df27f7206ded6bfb1628756792e0bcb30dda203a
yaoxipeng <yaoxipeng666@126.com>
2022-01-21 16:54:35 +0800

    新增用户注册模块以及创建文章路由

--- my_blog/userprofile/urls.py
+++ my_blog/userprofile/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     # 用户登出
     path('logout/', views.user_logout, name='logout'),
+    # 用户注册
+    path('register/', views.user_register, name='register'),
 ]
